5f1e42432f73530608bc51bd1b49e6db03a54e15
Blabz <connormcgeek@gmail.com>
2025-10-28 14:14:25 +1300

    Revert "eds.py ignore empty OD entries"
    This reverts commit eb66ca01d9f58fe01876439fdc614181df49ee39.

--- canopen/objectdictionary/eds.py
+++ canopen/objectdictionary/eds.py
@@ -57,13 +57,10 @@ def import_eds(source, node_id):
         logger.warn("eds file does not have a DeviceInfo section. This section is mandatory")
     else:
         for rate in [10, 20, 50, 125, 250, 500, 800, 1000]:
-            try:
-                baudPossible = int(
-                    eds.get("DeviceInfo", f"BaudRate_{rate}", fallback='0'), 0)
-                if baudPossible != 0:
-                    od.device_information.allowed_baudrates.add(rate*1000)
-            except ValueError:
-                pass
+            baudPossible = int(
+                eds.get("DeviceInfo", f"BaudRate_{rate}", fallback='0'), 0)
+            if baudPossible != 0:
+                od.device_information.allowed_baudrates.add(rate*1000)
 
         for t, eprop, odprop in [
             (str, "VendorName", "vendor_name"),
@@ -90,23 +87,16 @@ def import_eds(source, node_id):
                     setattr(od.device_information, odprop,
                             eds.get("DeviceInfo", eprop)
                             )
-            except (ValueError, NoOptionError):
+            except NoOptionError:
                 pass
 
     if eds.has_section("DeviceComissioning"):
-        try:
-            if val := eds.getint("DeviceComissioning", "Baudrate", fallback=None):
-                od.bitrate = val * 1000
-        except ValueError:
-            pass
+        if val := eds.getint("DeviceComissioning", "Baudrate", fallback=None):
+            od.bitrate = val * 1000
 
         if node_id is None:
-            try:
-                if val := eds.get("DeviceComissioning", "NodeID", fallback=None):
-                    node_id = int(val, base=0)
-            except ValueError:
-                pass
-                    
+            if val := eds.get("DeviceComissioning", "NodeID", fallback=None):
+                node_id = int(val, base=0)
         od.node_id = node_id
 
     for section in eds.sections():
@@ -115,14 +105,11 @@ def import_eds(source, node_id):
         if match is not None:
             for i in range(1, 8):
                 key = f"Dummy{i:04d}"
-                try:
-                    if eds.getint(section, key) == 1:
-                        var = ODVariable(key, i, 0)
-                        var.data_type = i
-                        var.access_type = "const"
-                        od.add_object(var)
-                except ValueError:
-                    pass
+                if eds.getint(section, key) == 1:
+                    var = ODVariable(key, i, 0)
+                    var.data_type = i
+                    var.access_type = "const"
+                    od.add_object(var)
 
         # Match indexes
         match = re.match(r"^[0-9A-Fa-f]{4}$", section)
